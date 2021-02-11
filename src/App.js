ce656810fbdb9f27d3f6192111a8bffb65e43a17
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-11 00:47:47 +0000

    Pull images from unsplash api, set them in state and display them in screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const accesKey = process.env.REACT_APP_BASE_URL;
+
 export default function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${accesKey}/photos/?client_id=${process.env.REACT_APP_ACCESS_KEY}`)
+      .then((res) => {
+        const { data } = res;
+        setPhotos(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!accesKey) {
+    return (
+      <a href="https://unsplash.com/documentation" className="error">
+        Required: get your unsplash key!
+      </a>
+    );
+  }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -12,9 +37,9 @@ export default function App() {
       </form>
 
       <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
+        {photos.map((image, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src={image.urls.small} alt={image.alt_description} />
           </div>
         ))}
       </div>
