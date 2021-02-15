f22a855c181befeb54259455eaee6f5733d38e63
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-15 23:58:52 +0000

    Load more photos when reaching bottom of screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 
 const accesKey = process.env.REACT_APP_BASE_URL;
@@ -8,16 +9,20 @@ export default function App() {
   const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
+    getPhotos();
+  }, []);
+
+  function getPhotos() {
     axios
       .get(`${accesKey}/photos/?client_id=${process.env.REACT_APP_ACCESS_KEY}`)
       .then((res) => {
         const { data } = res;
-        setPhotos(data);
+        setPhotos((photos) => [...photos, ...data]);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }
 
   if (!accesKey) {
     return (
@@ -35,14 +40,28 @@ export default function App() {
         <input type="text" placeholder="Search Unsplash..." />
         <button>Search</button>
       </form>
-
-      <div className="image-grid">
-        {photos.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.small} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={photos.length}
+        next={getPhotos}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {photos.map((image, index) => {
+            return (
+              <a
+                className="image"
+                key={index}
+                href={image.links.html}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={image.urls.small} alt={image.alt_description} />
+              </a>
+            );
+          })}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
